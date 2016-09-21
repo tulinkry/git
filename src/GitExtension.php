89b4461787599fb7f1c25a5225f71fa39b6b50d4
Kryštof Tulinger <k.tulinger@seznam.cz>
2016-09-21 14:04:23 +0200

    supporting multiple repositories

--- src/GitExtension.php
+++ src/GitExtension.php
@@ -12,34 +12,99 @@ use Nette\Utils\Validators;
 class GitExtension extends CompilerExtension
 {
 
-    public $defaults = array (
-        "username" => "tulinkry",
-        "repository" => null,
-        "branch" => "master",
-        "file" => "master.zip",
-        "key" => null,
+    private $defaults = array (
+        'repositories' => array(),
+        'url' => 'git'
     );
 
-    public function loadConfiguration () {
-        $config = $this -> validateConfig( $this -> defaults );
-        $builder = $this -> getContainerBuilder();
+    private $repositoryDefault = array (
+        'url' => null,
+        'username' => null,
+        'repository' => null,
+        'branch' => 'master',
+        'directory' => null,
+        'key' => null,
+        'flush' => false,
+    );
 
-        Validators::assertField( $config, 'username', 'string:1..', 'configuration of \'%\' in the git extension' );
-        Validators::assertField( $config, 'repository', 'string:1..', 'configuration of \'%\' in the git extension' );
-        Validators::assertField( $config, 'file', 'string:1..', 'configuration of \'%\' in the git extension' );
-        Validators::assertField( $config, 'branch', 'string:1..', 'configuration of \'%\' in the git extension' );
+
+
+    public function loadConfiguration () {
+        $config = $this->getConfig($this -> defaults);
+        $builder = $this->getContainerBuilder();
+
+        Validators::assertField( $config, 'url', 'string:1..', 'configuration of \'%\' in the git extension' );
+
+        if(count($config['repositories']) === 0) {
+            // single provider
+            $config['repositories'] ['default'] = $this->validateConfig($this->repositoryDefault, $this->config);
+
+            if(!isset($$config['repositories']['default']['directory'])) {
+                $config['repositories'] ['default']['directory'] = $builder->parameters['appDir'] . DIRECTORY_SEPARATOR . '..';
+            }
+
+            unset($config['repositories']['default']['repositories']);
+            foreach($this->repositoryDefault as $key => $value) {
+                if($key !== 'url') {
+                    unset($config[$key]);
+                }
+            }
+
+        } else {
+            foreach($this->repositoryDefault as $key => $value) {
+                if($key !== 'url' && isset($config[$key])) {
+                    throw new AssertionException("Configuration of '$key' in the git extension can't " .
+                                                 "be specified when using multiple repositories");
+                }
+            }
+        }
+
+        $config = $this->validateConfig($this->defaults, $config);
+
+        foreach($config['repositories'] as $name => &$repository) {
+
+            $repository = $this->validateConfig($this->repositoryDefault, $repository);
+
+            Validators::assertField( $repository, 'username', 'string:1..', 'configuration of \'%\' in the git extension' );
+            Validators::assertField( $repository, 'repository', 'string:1..', 'configuration of \'%\' in the git extension' );
+            Validators::assertField( $repository, 'flush', 'boolean', 'configuration of \'%\' in the git extension' );
+            Validators::assertField( $repository, 'directory', 'string:1..', 'configuration of \'%\' in the git extension' );
+
+            if(!isset($repository['branch']) || empty($repository['branch'])) {
+                $repository['branch'] = 'master';
+            }
+
+            if(!isset($repository['url']) || empty($repository['url'])) {
+                $repository['url'] = $this->config['url'];
+            }
+
+            $repository = (object) $repository;
+        }
+
+        ksort($config['repositories']);
 
         $builder -> addDefinition( $this -> prefix( "parameters" ) )
-                -> setClass( "Tulinkry\Git\Services\ParameterService", [$config ] );
+                -> setClass( "Tulinkry\Git\Services\ParameterService", [$config] );
     }
 
     public function beforeCompile () {
         $builder = $this -> getContainerBuilder();
 
+        $urls = array ( $this->config['url'] );
+        foreach($this->config['repositories'] as $name => &$repository) {
+            if(isset($repository['url']) &&
+                !empty($repository['url']) &&
+                !in_array($repository['url'], $urls)) {
+                $urls [] = $repository['url'];
+            }
+        }
+
         $router = $builder -> getByType( 'Nette\Application\IRouter' ) ?: 'router';
         if ( $builder -> hasDefinition( $router ) ) {
-            $builder -> getDefinition( $router )
-                    -> addSetup( '\Tulinkry\DI\GitExtension::modifyRouter(?)', [ '@self' ] );
+            foreach($urls as $url) {
+                $builder -> getDefinition( $router )
+                        -> addSetup( '\Tulinkry\DI\GitExtension::modifyRouter(?, ?)', [ $url, '@self' ] );
+            }
         }
 
         $presenterFactory = $builder -> getByType( 'Nette\Application\IPresenterFactory' ) ?: 'nette.presenterFactory';
@@ -51,14 +116,15 @@ class GitExtension extends CompilerExtension
         }
     }
 
-    public static function modifyRouter ( IRouter &$router ) {
+    public static function modifyRouter ( $url, IRouter &$router ) {
         if ( ! $router instanceof RouteList ) {
             throw new AssertionException( 'Your router should be an instance of Nette\Application\Routers\RouteList' );
         }
 
-        $router[] = $newRouter = new Route( "git", array ( 'module' => 'Git',
-            'presenter' => "Git",
-            'action' => 'default' ) );
+        $router[] = $newRouter = new Route( $url,
+            array ( 'module' => 'Git',
+                    'presenter' => "Git",
+                    'action' => 'default' ) );
 
         $lastKey = count( $router ) - 1;
         foreach ( $router as $i => $route ) {
--- src/Presenters/GitController.php
+++ src/Presenters/GitController.php
@@ -3,12 +3,19 @@
 namespace Tulinkry\Git;
 
 use Nette\Caching\IStorage;
+use Nette\IOException;
+use Nette\DI\Container;
+use Nette\Utils\FileSystem;
+use Nette\Caching\Cache;
+use Tracy\Debugger;
 use Tulinkry\Application\UI\Presenter;
 use Tulinkry\Git\Services\ParameterService;
 use Tulinkry\Zip\ZipArchiver;
 
 class GitController extends Presenter
 {
+    const TEMP_DIRECTORY = 'git-downloads';
+    const DEFAULT_DOWNLOAD_FILE = 'master.zip';
 
     /** @var ParameterService @inject */
     public $parameterService;
@@ -16,70 +23,225 @@ class GitController extends Presenter
     /** @var IStorage @inject */
     public $cache;
 
-    public function verifySignature ( $body ) {
+    /** @var Container @inject */
+    public $container;
 
-        if ( ! isset( $_SERVER[ "HTTP_X_HUB_SIGNATURE" ] ) )
+    /**
+     * Internal wrapper for hash_equals function for php < 5.3
+     * @param  string $hash1 hash 1
+     * @param  string $hash2 hash 2
+     * @return boolean        true if hash 1 and hash 2 are equal
+     */
+    private static function hash_equals($hash1, $hash2) {
+        if(function_exists('hash_equals')) {
+            return hash_equals($hash1, $hash2);
+        }
+        if(strlen($hash1) != strlen($hash2)) {
             return false;
+        } else {
+            $res = $hash1 ^ $hash2;
+            $ret = 0;
+            for($i = strlen($res) - 1; $i >= 0; $i--) {
+                $ret |= ord($res[$i]);
+            }
+            return !$ret;
+        }
+        return false;
+    }
+    /**
+     * Verifies the github signature for authenticated requests
+     *
+     * @param  string $key  shared key
+     * @param  string $body body of the github response
+     * @return boolean       true if the body is authenticated
+     */
+    public function verifySignature ( $key, $body ) {
+
+        if ( ! isset( $_SERVER[ "HTTP_X_HUB_SIGNATURE" ] ) ) {
+            Debugger::log("Updating from git: HTTP_X_HUB_SIGNATURE header is missing");
+            return false;
+        }
 
         list($algo, $hash) = explode( '=', $_SERVER[ 'HTTP_X_HUB_SIGNATURE' ], 2 ) + array ( '', '' );
 
         if ( ! in_array( $algo, hash_algos(), TRUE ) ) {
-            $this -> error( "Hash algorithm '$algo' is not supported." );
+            Debugger::log("Updating from git: hash algorithm '$algo' is not supported.");
+            return false;
         }
 
-        return hash_equals( $hash, hash_hmac( $algo, $body, $this -> parameterService -> key ) );
+        return self::hash_equals( $hash, hash_hmac( $algo, $body, $key ) );
     }
 
-    public function recursiveRmdir ( $dir ) {
-        $iterator = new \RecursiveIteratorIterator( new \RecursiveDirectoryIterator( $dir, \FilesystemIterator::SKIP_DOTS ), \RecursiveIteratorIterator::CHILD_FIRST );
-        foreach ( $iterator as $filename => $fileInfo ) {
-            if ( $fileInfo -> isDir() ) {
-                rmdir( $filename );
-            }
-            else {
-                unlink( $filename );
-            }
+    /**
+     * Downloads single repository
+     * @param array $repository repository configuration
+     */
+    private function downloadRepository($repository) {
+        $download_url = sprintf( "https://github.com/%s/%s/archive/%s.zip",
+                                    $repository -> username,
+                                    $repository -> repository,
+                                    $repository -> branch );
+
+        if ( ( $content = @file_get_contents( $download_url ) ) === FALSE ) {
+            throw new IOException("$download_url couldn't be downloaded.");
         }
-    }
 
-    public function actionDefault () {
-        if ( ($postdata = file_get_contents( "php://input" )) === FALSE )
-            $this -> error( "Cannot read input" );
+        $tempDir = $this->container->parameters['tempDir'] . DIRECTORY_SEPARATOR . self::TEMP_DIRECTORY;
+        $file = tempnam($tempDir,
+                        sprintf('%s-%s-%s.zip-',
+                                $repository -> username,
+                                $repository -> repository,
+                                $repository -> branch));
 
-        if ( ($decoded = json_decode( $postdata )) === FALSE )
-            $this -> error( "Cannot decode json data" );
-
-        if ( $this -> parameterService -> key && ! $this -> verifySignature( $postdata ) )
-            $this -> error( "Secret is needed to authenticate this request" );
-
-        $download_url = sprintf( "https://github.com/%s/%s/archive/%s", $this -> parameterService -> username, $this -> parameterService -> repository, $this -> parameterService -> file );
-
-        if ( ( $content = file_get_contents( $download_url ) ) === FALSE )
-            $this -> error( "Invalid input file" );
-
-        $file = APP_DIR . "/../" . $this -> parameterService -> file;
-
-        if ( file_put_contents( $file, $content ) === FALSE )
-            $this -> error( "Cannot save the data" );
+        FileSystem::write($file, $content);
+        FileSystem::createDir($repository->directory);
 
         $zip = new ZipArchiver;
-
-        $this -> template -> status = "failed";
-
-        $res = $zip -> open( $file );
-        if ( $res === TRUE ) {
-            $zip -> extractSubdirTo( APP_DIR . "/../", $this -> parameterService -> repository . "-" . $this -> parameterService -> branch );
-            $zip -> close();
-            //$this->cache->clean(array(Nette\Caching\Cache::ALL => true));
-            // clear cache
-            $this -> recursiveRmdir( APP_DIR . "/../temp/cache" );
-            @mkdir( APP_DIR . "/../temp/cache" );
-
-            $this -> template -> status = "saved";
+        if(($res = $zip -> open( $file )) !== TRUE) {
+            throw new IOException("The zipped file from $download_url couldn't be extracted.");
         }
-        else {
-            $this -> error( "cannot extract the zip archive" );
+
+        $zip -> extractSubdirTo($repository->directory, sprintf('%s-%s', $repository -> repository, $repository -> branch));
+        $zip -> close();
+    }
+
+    /**
+     * @return string maintenance content
+     */
+    private function getMaintenanceContent() {
+        $content = <<<EOF
+<?php
+
+require __DIR__ . DIRECTORY_SEPARATOR . '.maintenance.php';
+
+\$container = require __DIR__ . '/../app/bootstrap.php';
+
+\$container->getByType(Nette\Application\Application::class)->run();
+EOF;
+        $content .= "\n\n/** @random " . md5($content) . " */\n";
+        return $content;
+    }
+
+    /**
+     * Puts the page into the maintenance mode and backups the original index file
+     * @param  [type] $wwwDir path to www directory
+     */
+    private function shutdownPage($wwwDir) {
+        $content = $this->getMaintenanceContent();
+
+        FileSystem::copy($wwwDir . DIRECTORY_SEPARATOR . 'index.php', $wwwDir . DIRECTORY_SEPARATOR . 'index.php.backup');
+        // use $mode = NULL to bypass the permission problems
+        FileSystem::write($wwwDir . DIRECTORY_SEPARATOR . 'index.php', $content, $mode = NULL);
+    }
+
+    /**
+     * Turns on the page from the index backup.
+     * The new incoming index file is overwritten only when it does not exist or
+     * has the same content as the maintenance index
+     * @param  [type] $wwwDir path to www directory
+     */
+    private function turnonPage($wwwDir) {
+        $content = $this->getMaintenanceContent();
+
+        if(($oldIndex = @file_get_contents($wwwDir . DIRECTORY_SEPARATOR . 'index.php.backup')) === FALSE) {
+            // now this is bad
+            Debugger::log("Updating from git: Couldn't reproduce the old index file", Debugger::ERROR);
+            throw new IOException("Couldn't read '" . $wwwDir . DIRECTORY_SEPARATOR . "index.php.backup'.");
         }
+
+        $newIndex = @file_get_contents($wwwDir . DIRECTORY_SEPARATOR . 'index.php');
+
+
+        if($newIndex === FALSE || $newIndex === $content) {
+            // index has not changed (page is in maintenance)
+            // or the index has disappeared (somehow)
+            // => overwrite it with the old index
+            //
+            // use $mode = NULL to bypass the permission problems
+            FileSystem::write($wwwDir . DIRECTORY_SEPARATOR . 'index.php', $oldIndex, $mode = NULL);
+        }
+
+
+        FileSystem::delete($wwwDir . DIRECTORY_SEPARATOR . 'index.php.backup');
+    }
+
+    /**
+     * Handles the incoming request to the git route
+     * 1) Reads the input from github hook
+     * 2) Puts the page into maintenance
+     * 3) Downloads every repository from the configuration and extracts it into its directory
+     * 4) Puts the page into usual mode
+     */
+    public function actionDefault () {
+        set_time_limit(0);
+
+        $down = false;
+        $wwwDir = $this->container->parameters['wwwDir'];
+
+        if ( ($postdata = file_get_contents( "php://input" )) === FALSE ) {
+            Debugger::log('Updating from git: couldn\'t read input');
+            $this -> error( "Couldn't read input" );
+        }
+
+        if ( ($decoded = json_decode( $postdata )) === FALSE ) {
+            Debugger::log('Updating from git: couldn\'t decode json data');
+            $this -> error( "Couldn't decode json data" );
+        }
+
+
+        $auth = true;
+        foreach($this->parameterService->repositories as $name => $repository) {
+            if ( $repository->key && ! $this->verifySignature( $repository->key, $postdata ) ) {
+                Debugger::log("Updating from git: the repository {$repository->repository} requested key authentication, however it failed");
+                Debugger::log("Updating from git: secret is needed to authenticate this request for repository {$repository->repository}");
+                $auth = false;
+            }
+        }
+
+        if(!$auth) {
+            $this->error("Updating from git: secret is needed to authenticate this request for repository {$repository->repository}");
+        }
+
+        if(file_exists($wwwDir . DIRECTORY_SEPARATOR . '.maintenance.php')) {
+            try {
+                $this->shutdownPage($wwwDir);
+                $down = true;
+            } catch(IOException $e) {
+                Debugger::log("Updating from git (shutdown the page): " . $e->getMessage());
+            }
+        }
+
+        $errors = [];
+        foreach($this->parameterService->repositories as $name => $repository) {
+            try {
+                $this->downloadRepository($repository);
+            } catch(IOException $e) {
+                Debugger::log("Updating from git: syncing {$repository->repository} (named $name) failed");
+                Debugger::log("Updating from git: " . $e->getMessage());
+                $errors [$name] = $e->getMessage();
+            }
+        }
+
+        // clear cache
+        // does not work for latte because it manages the cache in its own way
+
+        $cache = new Cache($this->cache);
+        $cache->clean(array(Cache::ALL => true));
+
+        FileSystem::delete($this->container->parameters['tempDir'] . DIRECTORY_SEPARATOR . "cache");
+        FileSystem::delete($this->container->parameters['tempDir'] . DIRECTORY_SEPARATOR . self::TEMP_DIRECTORY);
+        FileSystem::createDir($this->container->parameters['tempDir'] . DIRECTORY_SEPARATOR . "cache");
+
+        if($down) {
+            try {
+                $this->turnonPage($wwwDir);
+            } catch(IOException $e) {
+                Debugger::log("Updating from git (turning on the page): " . $e->getMessage());
+            }
+        }
+
+        $this->sendJson(array('status' => 'finished',
+                              'errors' => $errors));
     }
 
 }
